015914f16d82ba5d83a35324dcae93295b51b1d2
Bishr AlAbbadi <bishralabbadi@gmail.com>
2017-03-11 11:56:46 +0300

    email auth done

--- socialBishux/SignInVC.swift
+++ socialBishux/SignInVC.swift
@@ -13,6 +13,12 @@ import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailField: FancyField!
+    
+    @IBOutlet weak var passwordField: FancyField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -53,6 +59,27 @@ class SignInVC: UIViewController {
             }
         })
     }
+    
+    @IBAction func signinTapped(_ sender: Any) {
+        
+        if let email = emailField.text, let password = passwordField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error == nil {
+                    print("BISHR: done auth EMAIL USER with firebase!")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if error == nil {
+                            print("BISHR: DONE create EMAIL USER with firebase!")
+                        } else {
+                            print("BISHR: unable to create EMAIL USER with firebase!")
+                        }
+                    })
+                }
+            })
+        }
+        
+    }
+    
 
 }
 
