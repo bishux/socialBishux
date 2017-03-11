4c1baf5024738d066a9e55a10452e2435d12cc36
Bishr AlAbbadi <bishralabbadi@gmail.com>
2017-03-11 15:30:02 +0300

    Setting custom cell view

--- socialBishux/FeedVC.swift
+++ socialBishux/FeedVC.swift
@@ -10,12 +10,31 @@ import UIKit
 import Firebase
 import SwiftKeychainWrapper
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableFeed: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableFeed.delegate = self
+        tableFeed.dataSource = self
+    
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
 
     @IBAction func signOutTapped(_ sender: Any) {
---
+++ socialBishux/postCell.swift
@@ -0,0 +1,38 @@
+//
+//  postCell.swift
+//  socialBishux
+//
+//  Created by Bishr Nebras AlAbbadi on 3/11/17.
+//  Copyright © 2017 BishrAlAbbadi. All rights reserved.
+//
+
+import UIKit
+
+class postCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var userLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.caption.setContentOffset(CGPoint.zero, animated: false)
+    }
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
