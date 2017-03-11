d775c93c5c0ed67ab9b39d041a0c22f5a5a67ac6
Bishr AlAbbadi <bishralabbadi@gmail.com>
2017-03-11 10:03:52 +0300

    sign in ui custom classes done

---
+++ socialBishux/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  socialBishux
+//
+//  Created by Bishr Nebras AlAbbadi on 3/11/17.
+//  Copyright © 2017 BishrAlAbbadi. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ socialBishux/FancyField.swift
@@ -0,0 +1,32 @@
+//
+//  FancyField.swift
+//  socialBishux
+//
+//  Created by Bishr Nebras AlAbbadi on 3/11/17.
+//  Copyright © 2017 BishrAlAbbadi. All rights reserved.
+//
+
+import UIKit
+
+class FancyField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 3.0
+
+        
+    }
+    
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10.0, dy: 5.0)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10.0, dy: 5.0)
+
+    }
+}
